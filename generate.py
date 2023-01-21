aa26816358d8fe8182b23886d78a727f84cce6dd
Fdeolive <fdeolive@uvm.edu>
2023-01-21 09:45:32 -0500

    changed the x,y,z

--- generate.py
+++ generate.py
@@ -1,10 +1,10 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-length=1
-width=2
-height=3
+x=1
+y=2
+z=1
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[width,length,height])
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[x,y,z])
 
 pyrosim.End()
