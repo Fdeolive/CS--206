8de600572bf5c515cd4a7c88e2076828b2dc0fff
Fdeolive <fdeolive@uvm.edu>
2023-01-21 15:10:30 -0500

    renamed the .sdf file

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("world.sdf")
 
 
 length=1
@@ -11,33 +11,23 @@ x=1
 y=1
 z=.5
 
-#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 #pyrosim.Send_Cube(name="Box2", pos=[2,1,2] , size=[length,width,height])
-for i in range (5):
-        for i in range(5):
-                for i in range(10):
-                        pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+# for i in range (5):
+#         for i in range(5):
+#                 for i in range(10):
+#                         pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
-                        width=width*.90
-                        length=length*.90
-                        height=height*.90
-                        z=z+1
-                z=.5
-                x=x+1
-                width=1
-                length=1
-                height=1
-        x=x-5
-        y=y+1        
-#for i in range(10):
-    #    pyrosim.Send_Cube(name="Box", pos=[x,y,i+1] , size=[length,width,height])
-     #   height=height*.90
-        
-      #  width=width*.90
+#                         width=width*.90
+#                         length=length*.90
+#                         height=height*.90
+#                         z=z+1
+#                 z=.5
+#                 x=x+1
+#                 width=1
+#                 length=1
+#                 height=1
+#         x=x-5
+#         y=y+1        
 
-       # lenght=length*.90
-        
-     
-       
-        
 pyrosim.End()
