932e553447e563577079dde3e3f6c2baba1b5b7e
Fdeolive <fdeolive@uvm.edu>
2023-01-21 09:29:30 -0500

    added lenght, widht, and height

--- generate.py
+++ generate.py
@@ -1,7 +1,10 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
+length=1
+width=2
+height=3
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[width,length,height])
 
 pyrosim.End()
